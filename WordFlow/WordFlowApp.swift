175d62c7df4a9b39b622cc0204a4a068fae6fab3
Rich Tape <richard.tape@ubc.ca>
2025-06-07 13:35:57 -0700

    Initial Commit

---
+++ WordFlow/WordFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordFlowApp.swift
+//  WordFlow
+//
+//  Created by Rich Tape on 2025-06-07.
+//
+
+import SwiftUI
+
+@main
+struct WordFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordFlowTests/WordFlowTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordFlowTests.swift
+//  WordFlowTests
+//
+//  Created by Rich Tape on 2025-06-07.
+//
+
+import Testing
+@testable import WordFlow
+
+struct WordFlowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
